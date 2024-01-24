df51a9fff9aa19e248cb4815095262d87f35fa23
Ángel Lareo <angel.lareo@gmail.com>
2024-01-24 13:00:07 +0100

    Initiated django app

---
+++ tribunales_evau/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tribunales_evau project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tribunales_evau.settings')
+
+application = get_asgi_application()

6f85175ca8b809f67a1ab36a4481d94b0cbfa210
Angel Lareo <angel.lareo@gmail.com>
2024-01-25 19:26:25 +0100

    Added DB

---
+++ tribunales_evau/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Sedes, Asignaturas, Evaluadores
+
+admin.site.register(Sedes)
+admin.site.register(Asignaturas)
+admin.site.register(Evaluadores)
